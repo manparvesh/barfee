8a08fd4aa465e021cb4edd50e07b0b3e3343bd3f
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-07-18 22:17:02 +0800

    Initial commit :tada:

---
+++ src/main/java/com/manparvesh/barfee/BarfeeApplication.java
@@ -0,0 +1,12 @@
+package com.manparvesh.barfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarfeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarfeeApplication.class, args);
+    }
+}
